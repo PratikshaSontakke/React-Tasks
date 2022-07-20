2dda4bbf5484bc12fb201dfb215a9f0ffd597a96
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-07-20 10:36:31 +0530

    Re-adding tasks

---
+++ wishlisttask/src/Components/Header.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import './Head.css'
+import Navbar from './Navbar'
+//import Card from "./Card";
+import ShowItem from './ItemView'
+//import WishList from './Wishlist';
+import Delete from './Delete';
+
+
+
+
+const Header = () =>{
+    const [deleteIcon, setDeleteIcon] = useState(false);
+    const [deleteIconForCard, setDeleteIconForCard] = useState(false);
+    const SelectAllCards =() =>{
+        if (deleteIconForCard===false)
+        setDeleteIconForCard(true);
+        else
+        setDeleteIconForCard(false);
+
+    }
+    return(
+        <>
+            <Navbar/>
+            <Delete cardSelect={SelectAllCards}/>
+            <ShowItem deleteicon={deleteIcon} setDelete={setDeleteIcon} deleteiconforCard={deleteIconForCard} />
+            
+            
+            
+        </>
+    )
+}
+
+
+
+export default Header;
