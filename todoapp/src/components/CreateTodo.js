0098c8e11777a147e496d1b106236f698ef8d0d1
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-07-28 18:15:45 +0530

    Added delete and done

--- todoapp/src/components/CreateTodo.js
+++ todoapp/src/components/CreateTodo.js
@@ -1,71 +1,101 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Todolist from "./Todolist";
 import swal from "sweetalert";
 //import "../styles/Styles.css";
 
 function Createtodo() {
-//Two useStates for title and done property
+  //Two useStates for title and done property
 
-const[todo, setTodo]=useState({title: "",  done: false})
-const[todoArr, setTodoArr]= useState([])
+  const [todo, setTodo] = useState({ title: "", done: false });
+  const [todoArr, setTodoArr] = useState([]);
 
-// to get todo's from local storage
-let todos=localStorage.hasOwnProperty("todos")? JSON.parse(localStorage.getItem("todos")):[]
+  // to get todo's from local storage
+  let todos = localStorage.hasOwnProperty("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
 
-// function for change in adding todo in the list
+  // function for change in adding todo in the list
 
-const onChange=(event)=>{
+  const onChange = (event) => {
     //destructuring objects in an event
 
-    let {value}=event.target 
-    let obj={}
-    obj["title"]=value
-    obj["done"]= false
-    setTodo(obj)
-}
+    let { value } = event.target;
+    let obj = {};
+    obj["title"] = value;
+    obj["done"] = false;
+    setTodo(obj);
+  };
 
-//Function which will work when add todo will be clicked
-const createTodo=(event)=>{
-
-  const {name}=event.target
-  if(event.key==="Enter" || name=== "addTodo"){
-    if (todo.title !== ""){
-      todos.unshift(todo)
-      localStorage.setItem('todos', JSON.stringify(todos))
-      setTodo({title: "", done: false})
-    }else{
-
-      swal("OOps!", "Please write todo first", "error")
+  //Function which will work when add todo will be clicked
+  const createTodo = (event) => {
+    const { name } = event.target;
+    if (event.key === "Enter" || name === "addTodo") {
+      if (todo.title !== "") {
+        todos.unshift(todo);
+        localStorage.setItem("todos", JSON.stringify(todos));
+        setTodo({ title: "", done: false });
+      } else {
+        swal( "Please write todo first", "error");
+      }
     }
-  }
-}
-//for making done property true
-const completeTodo=(i)=>{
-if (todos[i]["done"] !== true){
-  todos[i]["done"] = true
-  localStorage.setItem("todos", JSON.stringify(todos))
-  setTodoArr(todos)
-  swal ("Good job", "Todo completed", "sucess")
-}
+  };
 
-}
+  //for making done property true
+  const completeTodo = (i) => {
+    if (todos[i]["done"] !== true) {
+      todos[i]["done"] = true;
+      localStorage.setItem("todos", JSON.stringify(todos));
+      setTodoArr(todos);
+      swal("Good job", "Todo completed", "sucess");
+    }
+  };
+  //function for deleting todo
+  const deleteTodo=(i)=>{
+    swal({
+
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this file!",
+      buttons: true,
+      dangerMode: true
+    }).then(res=>{
+      if (res){
+        todos.splice(i, 1)
+        localStorage.setItem('todos', JSON.stringify(todos))
+        setTodoArr(todos)
+      }
+    })
+  };
+
+  
+
   return (
-    <> 
-    <div className="box">
-      <div className="text-end">
-        <h2> Add ToDo!!!</h2>
-      </div>
+    <>
+      <div className="box">
+        <div className="text-end">
+          <h2> Add ToDo!!!</h2>
+        </div>
 
-      <div className="text-addTodo">
-        <input type="text" name="todo" placeholder="Write here..." value={todo.title} onKeyPress={createTodo}onChange={onChange}/>
-        <button className="btn-addTodo" type="button" name="addTodo" onClick={createTodo}>
-          Add ToDo
-        </button>
+        <div className="text-addTodo">
+          <input
+            type="text"
+            name="todo"
+            placeholder="Write here..."
+            value={todo.title}
+            onKeyPress={createTodo}
+            onChange={onChange}
+          />
+          <button
+            className="btn-addTodo"
+            type="button"
+            name="addTodo"
+            onClick={createTodo}
+          >
+            Add ToDo
+          </button>
+        </div>
       </div>
-    </div>
-    <Todolist todoArr={todoArr}
-      completeTodo={completeTodo}
-    />
+      <Todolist todoArr={todoArr} completeTodo={completeTodo} deleteTodo={deleteTodo}
+      />
     </>
   );
 }
--- todoapp/src/components/Todolist.js
+++ todoapp/src/components/Todolist.js
@@ -1,89 +1,48 @@
 import React from "react";
+import "../styles/Styles.css";
 
-function Todolist() {
+function Todolist(props) {
+  const { completeTodo, deleteTodo } = props;
+  let todoArr =
+    props.todoArr.length > 0
+      ? props.todoArr
+      : JSON.parse(localStorage.getItem("todos"));
   return (
     <div className="todo-list">
       <div style={{ masHeight: "210px", overflowY: "auto" }}>
         <ul style={{ margin: 0, padding: 0, listStyle: "none" }}>
-          <li>
-            <div> Pay electricity bill </div>
-            <div className="icon">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "50px",
-                }}
-              >
-                <i title="Complete" className="fas fa-check-circle pointer" />
-                <i title="Delete" className="fas fa-trash-alt pointer" />
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div> Grocery shopping </div>
-            <div className="icon">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "50px",
-                }}
-              >
-                <i title="Complete" className="fas fa-check-circle pointer" />
-                <i title="Delete" className="fas fa-trash-alt pointer" />
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div> Complete tasks for the day </div>
-            <div className="icon">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "50px",
-                }}
-              >
-                <i title="Complete" className="fas fa-check-circle pointer" />
-                <i title="Delete" className="fas fa-trash-alt pointer" />
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div> Take doc appointment </div>
-            <div className="icon">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "50px",
-                }}
-              >
-                <i title="Complete" className="fas fa-check-circle pointer" />
-                <i title="Delete" className="fas fa-trash-alt pointer" />
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div> Read a book </div>
-            <div className="icon">
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "50px",
-                }}
-              >
-                <i title="Complete" className="fas fa-check-circle pointer" />
-                <i title="Delete" className="fas fa-trash-alt pointer" />
-              </div>
-            </div>
-          </li>
+          {todoArr && todoArr.length > 0
+            ? todoArr.map((el, i) => (
+                <li key={i}>
+                  <div className={el["done"] ? "line-through" : null}>
+                    {" "}
+                    {el.title}{" "}
+                  </div>
+                  <div className="icon">
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: "50px",
+                      }}
+                    >
+                    <button> Edit </button>
+                      <i
+                        title="Complete"
+                        onClick={() => completeTodo(i)}
+                        className={`fas fa-check-circle pointer ${
+                          el["done"] ? "green" : "blue"
+                        }`}
+                      />
+                      
+                      
+                      <i title="Delete" onClick={()=> deleteTodo(i)} className="fas fa-trash-alt pointer" />
+                    
+                    </div>
+                  </div>
+                </li>
+              ))
+            : null}
         </ul>
       </div>
     </div>
